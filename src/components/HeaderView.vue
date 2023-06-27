bca3ace7cf9f255e9767fb17a53be1231a76d15a
Qwikle <qwikle.ziko@gmail.com>
2023-06-27 09:58:22 +0200

    feat: add list in navigation

--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -1,5 +1,21 @@
 <script setup lang="ts">
 import { Bars3BottomRightIcon } from '@heroicons/vue/24/outline'
+import { ref } from 'vue'
+
+const menus = ref([
+  {
+    label: 'Accueil',
+    path: '/'
+  },
+  {
+    label: 'Connexion',
+    path: '/signin'
+  },
+  {
+    label: 'Inscription',
+    path: '/signup'
+  }
+])
 </script>
 <template>
   <header class="h-16 bg-slate-800 flex items-center justify-between px-5">
@@ -12,8 +28,15 @@ import { Bars3BottomRightIcon } from '@heroicons/vue/24/outline'
         />
       </RouterLink>
       <nav class="hidden md:block">
-        <ul>
-          <li><RouterLink to="/signin">Connexion</RouterLink></li>
+        <ul class="flex gap-x-2">
+          <li v-for="menu in menus" :key="menu.label">
+            <RouterLink
+              :to="menu.path"
+              class="flex items-center h-8 px-2 rounded hover:bg-stone-500"
+              active-class="bg-stone-500"
+              >{{ menu.label }}</RouterLink
+            >
+          </li>
         </ul>
       </nav>
     </div>
