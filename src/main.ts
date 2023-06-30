45c8c1c198106243b659038ad7a2817c7d12393b
Qwikle <qwikle.ziko@gmail.com>
2023-06-30 20:57:40 +0200

    feat: add router and alertStore to main store

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,26 @@
 import './assets/main.css'
 import VueAnimXYZ from '@animxyz/vue3'
 import '@animxyz/core'
-import { createApp } from 'vue'
+import { createApp, markRaw } from 'vue'
 import { createPinia } from 'pinia'
 import apiService from './services/api/api.service'
 import App from './App.vue'
 import router from './router'
 import { ApiKey } from './services/symbols'
+import type { Router } from 'vue-router'
+import { useAlertStore } from './stores/alert.store'
+
+const pinia = createPinia()
+
+pinia.use(({ store }) => {
+  store.$router = markRaw(router as Router)
+  store.$alert = useAlertStore()
+})
 
 const app = createApp(App)
 app.provide(ApiKey, apiService)
 app.use(VueAnimXYZ)
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
