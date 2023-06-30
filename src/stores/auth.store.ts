e7af9a0b588359d49e8f70a42c412be1a1b230af
Qwikle <qwikle.ziko@gmail.com>
2023-06-30 21:01:59 +0200

    feat: set the logic inside the actions

--- src/stores/auth.store.ts
+++ src/stores/auth.store.ts
@@ -1,22 +1,49 @@
 import authService, { type SignInInput, type SignUpInput } from '@/services/auth/auth.service'
 import { defineStore } from 'pinia'
+import type { User } from '@/models'
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     isLogged: false,
-    user: null
+    user: null as User | null
   }),
   actions: {
     async signIn({ email, password }: SignInInput) {
-      const data = await authService.signIn({ email, password })
-      this.isLogged = true
-      return 'ok'
+      try {
+        const user = await authService.signIn({ email, password })
+        this.user = user
+        console.log(this.user)
+        this.$alert.showAlert({
+          message: 'Vous êtes connecté',
+          type: 'success'
+        })
+        this.isLogged = true
+        this.$router.push({ name: 'Home' })
+      } catch (errors) {
+        this.$alert.showAlert({
+          message: errors.message,
+          type: 'error'
+        })
+      }
     },
 
     async signUp(form: SignUpInput) {
-      const data = await authService.signUp(form)
-      this.isLogged = true
-      return 'ok'
+      try {
+        const user = await authService.signUp(form)
+        this.user = user
+        this.$alert.showAlert({
+          message: 'Vous êtes connecté',
+          type: 'success'
+        })
+        this.isLogged = true
+        this.$router.push({ name: 'Home' })
+      } catch (errors) {
+        console.log(errors)
+        this.$alert.showAlert({
+          message: errors.message,
+          type: 'error'
+        })
+      }
     }
   }
 })
