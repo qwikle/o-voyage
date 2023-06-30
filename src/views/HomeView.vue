cf41a9e5ac0497b37028af5722ec44b878749be8
Qwikle <qwikle.ziko@gmail.com>
2023-06-30 21:03:45 +0200

    feat: WIP set the homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts"></script>
 <template>
-  <div>welcome HomeView</div>
+  <section class="bg-primary text-white -my-4 w-full -mx-5">
+    <h1>Home</h1>
+  </section>
 </template>
 
 <style scoped></style>
