424fb7471e251049dce7f53e2e77f5f877d0df48
Qwikle <qwikle.ziko@gmail.com>
2023-06-30 20:58:56 +0200

    refactor: remove alertstore and router

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -2,13 +2,9 @@
 import inputWidget from '@/components/widgets/inputWidget.vue'
 import ContainerFormWidget from '@/components/widgets/ContainerFormWidget.vue'
 import { useAuthStore } from '@/stores/auth.store'
-import { useRouter } from 'vue-router'
 import { ref } from 'vue'
-import { useAlertStore } from '@/stores/alert.store'
 
 const authStore = useAuthStore()
-const alertStore = useAlertStore()
-const router = useRouter()
 const forms = ref([
   {
     name: 'email',
@@ -38,14 +34,7 @@ async function submitForm(event: Event) {
   event.preventDefault()
   const email = forms.value[0].value
   const password = forms.value[1].value
-  const res = await authStore.signIn({ email, password })
-  if (res) {
-    alertStore.showAlert({
-      message: 'Vous êtes connecté',
-      type: 'info'
-    })
-    router.push({ name: 'Home' })
-  }
+  await authStore.signIn({ email, password })
 }
 </script>
 
@@ -65,15 +54,13 @@ async function submitForm(event: Event) {
         :autocomplete="form.autocomplete"
         :isPassword="form.isPassword"
       />
-      <div class="flex flex-col items-center">
-        <button
-          type="submit"
-          class="btn btn-primary w-full"
-          :disabled="forms[0].value === '' || forms[1].value === ''"
-        >
-          Se connecter
-        </button>
-      </div>
+      <button
+        type="submit"
+        class="btn btn-primary w-full"
+        :disabled="forms[0].value === '' || forms[1].value === ''"
+      >
+        Se connecter
+      </button>
     </form>
   </ContainerFormWidget>
 </template>
