eee8cbad39cf6c4943ae8030668f83c90a2879bb
Qwikle <qwikle.ziko@gmail.com>
2023-07-02 00:49:29 +0200

    refactor: add axios instance to store

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,10 +1,12 @@
 import 'pinia'
 import type { Router } from 'vue-router'
 import { useAlertStore } from './alert.store'
+import { AxiosInstance } from 'axios'
 
 declare module 'pinia' {
   export interface PiniaCustomProperties {
     $router: Router
     $alert: ReturnType<typeof useAlertStore>
+    $axios: AxiosInstance
   }
 }
