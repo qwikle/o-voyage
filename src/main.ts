aa9ccf8638c8ef0e82d2aa3d3260ea062d6ec0f1
Qwikle <qwikle.ziko@gmail.com>
2023-07-02 00:49:57 +0200

    feat: add meta, store nad axios to store

--- src/main.ts
+++ src/main.ts
@@ -3,24 +3,26 @@ import VueAnimXYZ from '@animxyz/vue3'
 import '@animxyz/core'
 import { createApp, markRaw } from 'vue'
 import { createPinia } from 'pinia'
-import apiService from './services/api/api.service'
+import axios from 'axios'
 import App from './App.vue'
 import router from './router'
-import { ApiKey } from './services/symbols'
 import type { Router } from 'vue-router'
 import { useAlertStore } from './stores/alert.store'
+import { createHead } from '@unhead/vue'
 
 const pinia = createPinia()
 
 pinia.use(({ store }) => {
   store.$router = markRaw(router as Router)
   store.$alert = useAlertStore()
+  store.$axios = markRaw(axios)
 })
 
 const app = createApp(App)
-app.provide(ApiKey, apiService)
+const head = createHead()
 app.use(VueAnimXYZ)
-app.use(pinia)
 app.use(router)
+app.use(pinia)
+app.use(head)
 
 app.mount('#app')
