9ceb605b80b9758043d629476336960f28798460
Qwikle <qwikle.ziko@gmail.com>
2023-07-04 22:35:09 +0200

    feat: add meta to protect routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { isAuth } from './guards/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -6,19 +7,45 @@ const router = createRouter({
     {
       path: '/',
       name: 'Home',
-      component: () => import('../views/HomeView.vue')
+      component: () => import('../views/HomeView.vue'),
+      meta: {
+        onlyGuest: false,
+        requiresAuth: false
+      }
     },
     {
       path: '/signin',
       name: 'Signin',
-      component: () => import('../views/SignInView.vue')
+      component: () => import('../views/SignInView.vue'),
+      meta: {
+        onlyGuest: true,
+        requiresAuth: false
+      }
     },
     {
       path: '/signup',
       name: 'Signup',
-      component: () => import('../views/SignUpView.vue')
+      component: () => import('../views/SignUpView.vue'),
+      meta: {
+        onlyGuest: true,
+        requiresAuth: false
+      }
     }
   ]
+})
+
+router.beforeEach((to) => {
+  const authenticated = isAuth()
+  if (to.meta.requiresAuth) {
+    if (!authenticated) {
+      return { name: 'Signin' }
+    }
+  }
+  if (to.meta.onlyGuest) {
+    if (authenticated) {
+      return { name: 'Home' }
+    }
+  }
 })
 
 export default router
