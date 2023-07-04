786be20e61300e62c3504a6b57d307f180907528
Qwikle <qwikle.ziko@gmail.com>
2023-07-04 20:05:35 +0200

    style: change button color and header height

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -14,7 +14,7 @@ const menus = ref([
 ])
 </script>
 <template>
-  <header class="h-16 bg-slate-800 flex items-center justify-between px-5">
+  <header class="h-14 bg-slate-800 flex items-center justify-between px-5">
     <div class="flex items-center gap-x-4 text-white">
       <RouterLink to="/">
         <img
@@ -28,8 +28,8 @@ const menus = ref([
           <li v-for="menu in menus" :key="menu.label">
             <RouterLink
               :to="menu.path"
-              class="flex items-center h-8 px-2 rounded hover:bg-red-200 hover:text-slate-800"
-              active-class="bg-red-200 text-slate-800"
+              class="flex items-center py-1 px-2 rounded hover:bg-white hover:text-slate-800"
+              active-class="bg-white text-slate-800"
               >{{ menu.label }}</RouterLink
             >
           </li>
