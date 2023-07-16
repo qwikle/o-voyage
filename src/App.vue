37b6f96106ccf5a1f27a33655b22bc0025ac178c
Qwikle <qwikle.ziko@gmail.com>
2023-07-16 17:07:24 +0200

    refactor: rename alertComponent

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import HeaderComponent from './components/HeaderComponent.vue'
-import AlertWidget from './components/alertComponent.vue'
+import AlertWidget from './components/AlertComponent.vue'
 </script>
 
 <template>
