4e2ced294a30b1090723e9590e3464210704e278
Qwikle <qwikle.ziko@gmail.com>
2023-07-16 17:08:37 +0200

    refactor: move dialog into a DialogComponent

--- src/views/TripsView.vue
+++ src/views/TripsView.vue
@@ -1,15 +1,9 @@
 <script setup lang="ts">
 import { useHead } from '@unhead/vue'
 import { PlusIcon } from '@heroicons/vue/24/outline'
-import {
-  Dialog,
-  DialogTitle,
-  DialogPanel,
-  DialogDescription,
-  TransitionRoot,
-  TransitionChild
-} from '@headlessui/vue'
 import { ref } from 'vue'
+import DialogComponent from '@/components/DialogComponent.vue'
+import TravelFormWidget from '@/components/widgets/TravelFormWidget.vue'
 useHead({
   title: 'Mes voyages',
   meta: [
@@ -41,35 +35,9 @@ function setIsOpen(value: boolean) {
     <button class="self-end btn btn-sm btn-primary hidden lg:block" @click="setIsOpen(true)">
       Créer un voyage
     </button>
-    <TransitionRoot :show="isOpen" as="template">
-      <Dialog @close="setIsOpen">
-        <TransitionChild
-          enter="duration-300 ease-out"
-          enter-from="opacity-0"
-          enter-to="opacity-100"
-          leave="duration-200 ease-in"
-          leave-from="opacity-100"
-          leave-to="opacity-0"
-        >
-          <div
-            class="fixed inset-0 flex items-center justify-center p-4 bg-black/30"
-            aria-hidden="true"
-          >
-            <DialogPanel class="shadow mx-auto lg:w-1/2 rounded-xl bg-white">
-              <div class="flex flex-col gap-4">
-                <DialogTitle class="text-center text-2xl font-bold text-slate-700">
-                  Créer un voyage
-                </DialogTitle>
-                <DialogDescription class="text-center text-slate-700">
-                  Créez un voyage et invitez vos amis et votre famille à vous rejoindre dans votre
-                  périple !
-                </DialogDescription>
-              </div>
-            </DialogPanel>
-          </div>
-        </TransitionChild>
-      </Dialog>
-    </TransitionRoot>
+    <DialogComponent :show="isOpen" title="Créer un voyage" @close="setIsOpen">
+      <TravelFormWidget />
+    </DialogComponent>
   </div>
 </template>
 <style scoped></style>
