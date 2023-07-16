024100de1acb7b9a97ce3edb90ce96e951e8a65d
Qwikle <qwikle.ziko@gmail.com>
2023-07-16 18:36:34 +0200

    feat: create widget placeholder if no travel

---
+++ src/components/widgets/NoTravelWidget.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import departing from '@/assets/undraw/departing.svg'
+</script>
+<template>
+  <figure class="mx-auto pt-10 w-full relative lg:w-1/2">
+    <figcaption class="absolute text-xs top-24 lg:text-lg font-bold italic lg:top-28 left-0">
+      Vous n'avez pas encore de voyage
+    </figcaption>
+    <img :src="departing" alt="" class="" />
+  </figure>
+</template>
+<style scoped></style>
