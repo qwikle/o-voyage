be55e6752f251cbbf56c32350b1333786de2bc2a
Qwikle <qwikle.ziko@gmail.com>
2023-07-16 22:15:08 +0200

    feat: add travel store

---
+++ src/stores/travel.store.ts
@@ -0,0 +1,31 @@
+import travelService, { type TravelInput } from '@/services/travel/travel.service'
+import { defineStore } from 'pinia'
+import type { Travel } from '@/models'
+
+export const useTravelStore = defineStore('travel', {
+  state: () => ({
+    travels: [] as Travel[]
+  }),
+  actions: {
+    async getTravels() {
+      this.travels = await travelService.getTravelList()
+    },
+    async createTravel(travelForm: TravelInput) {
+      travelForm.organizerId = this.$auth.user?.id
+      try {
+        const travel = await travelService.createTravel(travelForm)
+        this.travels.push(travel)
+        this.$alert.showAlert({
+          message: 'Le voyage a bien été créé',
+          type: 'success'
+        })
+      } catch (error) {
+        this.$alert.showAlert({
+          // @ts-ignore
+          message: error.message,
+          type: 'error'
+        })
+      }
+    }
+  }
+})
