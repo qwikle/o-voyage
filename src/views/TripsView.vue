82c201c0572d69c3ada539e9347417f095ba604b
Qwikle <qwikle.ziko@gmail.com>
2023-07-16 22:15:27 +0200

    feat: WIP connect form to store

--- src/views/TripsView.vue
+++ src/views/TripsView.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
+import { onMounted, computed } from 'vue'
 import { useHead } from '@unhead/vue'
 import { PlusIcon } from '@heroicons/vue/24/outline'
 import { ref } from 'vue'
 import DialogComponent from '@/components/DialogComponent.vue'
 import TravelFormWidget from '@/components/widgets/TravelFormWidget.vue'
 import NoTravelWidget from '@/components/widgets/NoTravelWidget.vue'
+import { useTravelStore } from '@/stores/travel.store'
+import type { Travel } from '@/models'
+import WavesSvgWidget from '@/components/widgets/svg/WavesSvgWidget.vue'
+import type { TravelInput } from '@/services/travel/travel.service'
 useHead({
   title: 'Mes voyages',
   meta: [
@@ -22,6 +27,18 @@ const isOpen = ref(false)
 function setIsOpen(value: boolean) {
   isOpen.value = value
 }
+const travels = computed<Travel[]>(() => {
+  return travelStore.travels
+})
+const travelStore = useTravelStore()
+onMounted(async () => {
+  await travelStore.getTravels()
+})
+
+async function createTravel(travel: TravelInput) {
+  travel.from = 'France'
+  console.log(travel)
+}
 </script>
 <template>
   <div class="w-full pt-10 flex flex-col gap-4 px-4 overflow-x-auto">
@@ -37,9 +54,18 @@ function setIsOpen(value: boolean) {
       Créer un voyage
     </button>
     <DialogComponent :show="isOpen" title="Créer un voyage" @close="setIsOpen">
-      <TravelFormWidget isDialog />
+      <TravelFormWidget isDialog @submit="createTravel" />
     </DialogComponent>
-    <NoTravelWidget />
+    <NoTravelWidget v-if="!travels.length" />
+    <div class="grid grid-flow-row grid-cols-1 lg:grid-cols-4 gap-4 p-4">
+      <div
+        class="rounded-xl bg-slate-700 text-white shadow flex flex-col"
+        v-for="(travel, index) in travels"
+        :key="index"
+      >
+        <WavesSvgWidget class="h-50 w-50 fill-blue-500" />
+      </div>
+    </div>
   </div>
 </template>
 <style scoped></style>
