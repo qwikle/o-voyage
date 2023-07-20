dd7013676844b6e4f2e4fd4f15a4e2e2b8ffb4ad
Qwikle <qwikle.ziko@gmail.com>
2023-07-20 22:54:38 +0200

    feat: add route in global store to retrieve query

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,13 @@ import type { Router } from 'vue-router'
 import { useAlertStore } from './stores/alert.store'
 import { createHead } from '@unhead/vue'
 import { useAuthStore } from './stores/auth.store'
+import { useRoute } from 'vue-router'
 
 const pinia = createPinia()
 
 pinia.use(({ store }) => {
   store.$router = markRaw(router as Router)
+  store.$route = useRoute()
   store.$alert = useAlertStore()
   store.$auth = useAuthStore()
   store.$axios = markRaw(axios)
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -3,10 +3,12 @@ import type { Router } from 'vue-router'
 import { AxiosInstance } from 'axios'
 import type { AlertStore } from './stores/alert.store'
 import type { AuthStore } from './stores/auth.store'
+import type { RouteLocationNormalizedLoaded } from 'vue-router'
 
 declare module 'pinia' {
   export interface PiniaCustomProperties {
     $router: Router
+    $route: RouteLocationNormalizedLoaded
     $alert: AlertStore
     $auth: AuthStore
     $axios: AxiosInstance
