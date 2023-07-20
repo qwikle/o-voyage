d4aaacc0a957eb19e3789ac7b6367f8a0990bf89
Qwikle <qwikle.ziko@gmail.com>
2023-07-20 23:03:02 +0200

    feat: add models to index

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,6 @@
 import type { User } from './User'
-import type { Travel } from './Travel'
+import { type ITravel, Travel } from './Travel'
+import type { Traveler } from './Traveler'
+import { type ITravelInput, TravelInput } from './inputs/travel.input'
 
-export type { User, Travel }
+export { type User, type ITravel, type Traveler, type ITravelInput, TravelInput, Travel }
