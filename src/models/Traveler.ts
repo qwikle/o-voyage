ecf52689804d03404a7195fe743bc55b038a2605
Qwikle <qwikle.ziko@gmail.com>
2023-07-22 17:22:42 +0200

    feat: create class traveler

--- src/models/Traveler.ts
+++ src/models/Traveler.ts
@@ -1,6 +1,25 @@
-export interface Traveler {
+export interface ITraveler {
   id: number
   firstname: string
   lastname: string
   email: string
+  fullname: string
+}
+
+export class Traveler implements ITraveler {
+  id: number
+  firstname: string
+  lastname: string
+  email: string
+
+  constructor(traveler: ITraveler) {
+    this.id = traveler.id
+    this.firstname = traveler.firstname
+    this.lastname = traveler.lastname
+    this.email = traveler.email
+  }
+
+  get fullname(): string {
+    return `${this.firstname} ${this.lastname}`
+  }
 }
