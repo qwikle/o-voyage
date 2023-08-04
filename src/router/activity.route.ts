5da3ea5cd80e88ebf5f3c3ebe3dc4152edcfbc50
Qwikle <qwikle.ziko@gmail.com>
2023-08-04 16:29:54 +0200

    feat: create activitiy route file

---
+++ src/router/activity.route.ts
@@ -0,0 +1,19 @@
+export const activityRoutes = [
+  {
+    path: ':date([1-2][0-9]{3}-[0-1][0-9]-[0-3][0-9])',
+    name: 'TripDate',
+    component: () => import('../views/trips/TripDateView.vue'),
+    meta: {
+      onlyGuest: false,
+      requiresAuth: true
+    },
+    beforeEnter: async (to: { params: { slug: string; date: string } }) => {
+      const { useTravelStore } = await import('@/stores/travel.store')
+      const travel = useTravelStore().travel!
+      const exists = travel.dateList.find((date) => date.value === to.params.date)
+      if (!exists) {
+        return { name: 'NotFound' }
+      }
+    }
+  }
+]
