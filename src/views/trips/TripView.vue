856d83983ff590f296e97af46137e042c4fdef4d
Qwikle <qwikle.ziko@gmail.com>
2023-08-04 16:30:55 +0200

    feat: move select travelday to a new file and add routes to trip

--- src/views/trips/TripView.vue
+++ src/views/trips/TripView.vue
@@ -2,10 +2,9 @@
 import { useTravelStore } from '@/stores/travel.store'
 import { useHead } from '@unhead/vue'
 import DashboardWidget from '@/components/widgets/DashboardWidget.vue'
-import { ref, watch } from 'vue'
-import SelectTravelDayWidget from '@/components/widgets/SelectTravelDayWidget.vue'
-import { useRouter, useRoute } from 'vue-router'
-import { DateTime } from 'luxon'
+import { type RouteLocationRaw } from 'vue-router'
+
+import { Cog8ToothIcon, HomeIcon, SquaresPlusIcon } from '@heroicons/vue/24/outline'
 const travel = useTravelStore().travel!
 useHead({
   title: `${travel.title}`,
@@ -17,29 +16,50 @@ useHead({
   ]
 })
 
-const route = useRoute()
-const router = useRouter()
+const paths: Path[] = [
+  {
+    route: { name: 'TripOverview', params: { slug: travel.slug } },
+    name: 'Général',
+    icon: HomeIcon
+  },
+  {
+    route: { name: 'TripActivities', params: { slug: travel.slug } },
+    name: 'Activités',
+    icon: SquaresPlusIcon
+  },
+  {
+    route: { name: 'TripSettings', params: { slug: travel.slug } },
+    name: 'Paramètres',
+    icon: Cog8ToothIcon
+  }
+]
 
-const actualDate =
-  travel.dateList.find((date) => date.value === route.params.date) ??
-  travel.dateList.find((date) => date.value === DateTime.now().toFormat('yyyy-MM-dd')) ??
-  travel.dateList[0]
-
-const selectedDate = ref<{ day: number; date: string; value: string }>(actualDate)
-watch(selectedDate, (value) => {
-  router.push({ name: 'TripDate', params: { date: value.value } })
-})
+interface Path {
+  route: RouteLocationRaw
+  name: string
+  icon: any
+}
 </script>
 <template>
   <div class="flex flex-col gap-y-4 p-4">
     <DashboardWidget :travel="travel" />
-    <SelectTravelDayWidget
-      :dates="travel.dateList"
-      :startedDate="travel.departureDate"
-      :arrivalDate="travel.arrivalDate"
-      v-model="selectedDate"
-    />
-    <RouterView></RouterView>
+    <div class="h-12 w-full flex items-center gap-x-4">
+      <RouterLink
+        v-for="(path, index) in paths"
+        :key="index"
+        :to="path.route"
+        class="w-32 flex justify-center gap-x-2 items-center h-10 rounded-lg text-slate-700 hover:bg-gray-200"
+        activeClass="bg-gray-200"
+      >
+        <component :is="path.icon" class="h-5 w-5" />
+        {{ path.name }}</RouterLink
+      >
+    </div>
+    <RouterView v-slot="{ Component }">
+      <XyzTransition appear mode="out-in" xyz="fade duration-3">
+        <component :is="Component" />
+      </XyzTransition>
+    </RouterView>
   </div>
 </template>
 <style scoped></style>
---
+++ src/views/trips/activities/ActivitiesView.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import SelectTravelDayWidget from '@/components/widgets/SelectTravelDayWidget.vue'
+import { useRouter, useRoute } from 'vue-router'
+import { DateTime } from 'luxon'
+import { useTravelStore } from '@/stores/travel.store'
+
+const travel = useTravelStore().travel!
+
+const route = useRoute()
+const router = useRouter()
+
+const actualDate =
+  travel.dateList.find((date) => date.value === route.params.date) ??
+  travel.dateList.find((date) => date.value === DateTime.now().toFormat('yyyy-MM-dd')) ??
+  travel.dateList[0]
+
+const selectedDate = ref<{ day: number; date: string; value: string }>(actualDate)
+watch(selectedDate, (value) => {
+  router.push({ name: 'TripDate', params: { date: value.value } })
+})
+</script>
+<template>
+  <div class="flex flex-col">
+    <SelectTravelDayWidget
+      :dates="travel.dateList"
+      :startedDate="travel.departureDate"
+      :arrivalDate="travel.arrivalDate"
+      v-model="selectedDate"
+    />
+
+    <RouterView v-slot="{ Component }">
+      <XyzTransition appear mode="out-in" xyz="fade duration-3">
+        <component :is="Component" />
+      </XyzTransition>
+    </RouterView>
+  </div>
+</template>
+<style scoped></style>
