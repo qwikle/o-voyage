00cc4f612e748b5233d34c861b708e8da98fb3e8
Qwikle <qwikle.ziko@gmail.com>
2023-08-08 01:18:20 +0200

    feat: :construction: create dynamic form

---
+++ src/components/widgets/forms/DynamicFormWidget.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { Form } from 'vee-validate'
+import InputWidget, { type InputWidgetProps } from '../inputs/InputWidget.vue'
+defineProps<ISchema>()
+
+interface ISchema {
+  schema: {
+    fields: InputWidgetProps[]
+    submitLabel: string
+  }
+}
+</script>
+<template>
+  <Form class="flex flex-col gap-8">
+    <InputWidget
+      v-for="field in schema.fields"
+      :key="field.name"
+      :name="field.name"
+      :label="field.label"
+      :type="field.type"
+      :value="field.value"
+      :placeholder="field.placeholder"
+      :required="field.required"
+      :disabled="field.disabled"
+      :autocomplete="field.autocomplete"
+      :isPassword="field.isPassword"
+      :min="field.min"
+      :max="field.max"
+      :rules="field.rules"
+    />
+    <button
+      type="submit"
+      class="h-12 px-2 bg-primary rounded-lg text-white font-semibold hover:bg-primary-dark"
+    >
+      {{ schema.submitLabel }}
+    </button>
+  </Form>
+</template>
+<style scoped></style>
