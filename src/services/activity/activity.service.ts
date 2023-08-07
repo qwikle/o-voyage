6ef80c61a26f38c734396d47b49dc053eb52b3fc
Qwikle <qwikle.ziko@gmail.com>
2023-08-07 22:40:49 +0200

    feat: create activity service with first method by Date

--- src/services/activity/activity.service.ts
+++ src/services/activity/activity.service.ts
@@ -1,3 +1,26 @@
+import type { IActivity } from '@/models'
+import { Activity } from '@/models/activity'
 import { ClientService } from '@/services/api/client.service'
 
-export class ActivityService extends ClientService {}
+class ActivityService extends ClientService {
+  async getActivityByDate(date: string, travelId: number): Promise<IActivity[]> {
+    const { activities } = (await this.client.query({
+      query: `query Query($date: Date!, $activitiesByDateId: Int!) {
+  activitiesByDate(date: $date, id: $activitiesByDateId) {
+    id
+    name
+    price
+    location
+    members
+    time
+    travelId
+    categoryId
+  }
+}`,
+      variables: { date, activitiesByDateId: travelId }
+    })) as unknown as { activities: IActivity[] }
+    return activities ? activities.map((activity) => new Activity(activity)) : []
+  }
+}
+
+export default new ActivityService()
