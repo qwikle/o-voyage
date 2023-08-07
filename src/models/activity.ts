057fc8b5ea302387afba32a73097c69a71af6821
Qwikle <qwikle.ziko@gmail.com>
2023-08-07 22:38:20 +0200

    feat: create activity class

--- src/models/activity.ts
+++ src/models/activity.ts
@@ -1,4 +1,4 @@
-import type { ITravel, ICategory } from '.'
+import type { ITravel, ICategory } from '@/models'
 
 export interface IActivity {
   id: number
@@ -10,6 +10,34 @@ export interface IActivity {
   date: string
   travelId: number
   categoryId: number
-  travel: ITravel
-  category: ICategory
+  travel?: ITravel
+  category?: ICategory
+}
+
+export class Activity implements IActivity {
+  id: number
+  name: string
+  price: number
+  location: string
+  members: number
+  time: string
+  date: string
+  travelId: number
+  categoryId: number
+  travel?: ITravel
+  category?: ICategory
+
+  constructor(activity: IActivity) {
+    this.id = activity.id
+    this.name = activity.name
+    this.price = activity.price
+    this.location = activity.location
+    this.members = activity.members
+    this.time = activity.time
+    this.date = activity.date
+    this.travelId = activity.travelId
+    this.categoryId = activity.categoryId
+    this.travel = activity.travel
+    this.category = activity.category
+  }
 }
