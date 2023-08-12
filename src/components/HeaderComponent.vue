41f5f1726cb100a1aa82f5279e60bf4a5e9a5f7a
Qwikle <qwikle.ziko@gmail.com>
2023-08-12 23:22:04 +0200

    feat: add menu for mobile

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import { Bars3BottomRightIcon } from '@heroicons/vue/24/outline'
-import { computed } from 'vue'
+import { Bars3BottomRightIcon, XMarkIcon } from '@heroicons/vue/24/outline'
+import { computed, ref } from 'vue'
 import { useAuthStore } from '@/stores/auth.store'
 import { useRoute } from 'vue-router'
-
 const route = useRoute()
 const authStore = useAuthStore()
+
+const isOpen = ref(false)
 
 const menus = computed(() => {
   if (authStore.isAuthenticated) {
@@ -35,6 +36,8 @@ const menus = computed(() => {
     }
   ]
 })
+
+defineEmits(['isOpen', 'update:modelValue'])
 </script>
 <template>
   <header class="h-20 bg-slate-800 flex items-center justify-between px-5">
@@ -69,13 +72,40 @@ const menus = computed(() => {
       </nav>
     </div>
     <div class="flex items-center gap-x-2">
-      <button aria-label="ouvrir le menu" class="h-10 w-10 flex hover:rounded group lg:hidden">
-        <Bars3BottomRightIcon
-          class="text-white h-full w-full group-hover:bg-slate-500 rounded group-active:bg-slate-600"
-        />
-      </button>
+      <label
+        :aria-label="isOpen ? 'fermer le menu' : 'ouvrir le menu'"
+        class="hover:bg-gray-600 rounded-full group md:hidden swap swap-rotate"
+      >
+        <input type="checkbox" v-model="isOpen" />
+        <Bars3BottomRightIcon class="h-10 w-10 text-white swap-off" />
+        <XMarkIcon class="h-10 w-10 text-white swap-on" />
+      </label>
     </div>
   </header>
+  <XyzTransition appear mode="out-in" xyz="up duration-2">
+    <ul
+      v-if="isOpen"
+      class="w-full bg-slate-800 text-white flex flex-col px-4 pb-4 gap-y-3 md:hidden"
+    >
+      <li v-for="menu in menus" :key="menu.label" @click="isOpen = false">
+        <button
+          aria-label="se déconnecter"
+          v-if="menu.path === '/signout'"
+          @click="authStore.signOut"
+          class="flex items-center text-lg font-semibold py-1 px-2 rounded hover:bg-white hover:text-slate-800"
+        >
+          {{ menu.label }}
+        </button>
+        <RouterLink
+          v-else
+          :to="menu.path"
+          class="flex items-center text-lg font-semibold py-1 px-2 rounded hover:bg-white hover:text-slate-800"
+          exact-active-class="bg-white text-slate-800"
+          >{{ menu.label }}</RouterLink
+        >
+      </li>
+    </ul>
+  </XyzTransition>
 </template>
 
 <style scoped></style>
