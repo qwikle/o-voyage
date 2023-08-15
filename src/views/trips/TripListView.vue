0addb764e57567f8a2b7b33b5eca66766ff21674
Qwikle <qwikle.ziko@gmail.com>
2023-08-15 13:37:10 +0200

    feat: add travel if created from homeView

--- src/views/trips/TripListView.vue
+++ src/views/trips/TripListView.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import { useHead } from '@unhead/vue'
 import { PlusIcon } from '@heroicons/vue/24/outline'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import DialogComponent from '@/components/DialogComponent.vue'
 import TravelFormWidget from '@/components/widgets/forms/TravelFormWidget.vue'
 import NoTravelWidget from '@/components/widgets/NoTravelWidget.vue'
 import { useTravelStore } from '@/stores/travel.store'
 import { TravelInput, type ITravelInput } from '@/models'
 import TripWidget from '@/components/widgets/TripWidget.vue'
+import { useRoute, type LocationQuery } from 'vue-router'
 useHead({
   title: 'Mes voyages',
   meta: [
@@ -22,6 +23,7 @@ useHead({
   ]
 })
 const travelStore = useTravelStore()
+const route = useRoute()
 const isOpen = ref(false)
 function setIsOpen(value: boolean) {
   isOpen.value = value
@@ -32,6 +34,19 @@ async function createTravel(travel: ITravelInput) {
   await travelStore.createTravel(TravelInput.toJSON(travel))
   setIsOpen(false)
 }
+
+function checkQueryProperty(query: LocationQuery) {
+  const properties = ['to', 'departureDate', 'arrivalDate', 'budget', 'title', 'numberOfTravelers']
+
+  return properties.every((property) => Object.prototype.hasOwnProperty.call(query, property))
+}
+
+onMounted(async () => {
+  if (checkQueryProperty(route.query)) {
+    await travelStore.createTravel(TravelInput.toJSON(route.query as unknown as ITravelInput))
+    travelStore.$router.push({ name: 'TripList' })
+  }
+})
 </script>
 <template>
   <div class="w-full pt-10 flex flex-col gap-4 px-4 overflow-x-auto">
@@ -53,8 +68,11 @@ async function createTravel(travel: ITravelInput) {
     <DialogComponent :show="isOpen" title="Créer un voyage" @close="setIsOpen">
       <TravelFormWidget isDialog @submit="createTravel" @close="setIsOpen(false)" />
     </DialogComponent>
-    <NoTravelWidget v-if="!travels.length" />
+    <XyzTransition appear mode="out-in" xyz="fade duration-3" v-if="!travels.length">
+      <NoTravelWidget />
+    </XyzTransition>
     <XyzTransitionGroup
+      v-else
       class="flex flex-wrap gap-4 p-4 w-full h-full"
       appear
       xyz="fade flip-left origin-left duration-3 appear-stagger"
